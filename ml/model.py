333c1fc1d0cc85f41db2ae81553c995ea1a0e863
erinlmiller <erinthered324@gmail.com>
2024-06-18 22:12:36 -0400

    implement ml pipline

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,6 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
@@ -21,7 +20,7 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    #TODO: implement the function
+    # implement the function
 
     model = RandomForestClassifier()
 
@@ -68,7 +67,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
+    # implement the function
 
     preds = model.predict(X)
 
@@ -86,7 +85,7 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
+    # implement the function
 
     with open(path, 'wb') as f:
         pickle.dump(model, f)
@@ -95,7 +94,7 @@ def save_model(model, path):
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
+    # implement the function
 
     with open(path, 'rb') as f:
         model = pickle.load(f)
@@ -141,18 +140,15 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    # implement the function
 
-    data_sliced = data[data[column_name] == slice_value]
+    sliced_data = data[data[column_name] == slice_value]
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
 
-        data_sliced,
+        sliced_data,
         categorical_features=categorical_features,
         label=label,
-        encoder=encoder
+        encoder=encoder,
         lb=lb,
         training=False
 
